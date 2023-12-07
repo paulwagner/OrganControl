16cebd296474c6308ae3899aa3779aac48f02873
Paul Wagner <github@pwagner.de>
2023-12-07 22:21:10 +0100

    Fixed pins in KeystackControl

--- KeystackControl/Pins.cpp
+++ KeystackControl/Pins.cpp
@@ -2,4 +2,4 @@
 
 const int PINS_SWITCHES[] = {10, 9, 8}; // DATA, CLOCK, LATCH
 
-const int PINS_LEDS[] =     {4, 3, 2}; // DATA, CLOCK, LATCH
+const int PINS_LEDS[] =     {2, 3, 4}; // DATA, CLOCK, LATCH
